010cdda4987357cd2cfc774ca5362e70f129919a
Daniel Maxwell <dsm_direct@yahoo.com>
2018-07-06 16:16:10 -0400

    Edits (07.06.18).

--- src/learn_pandas.py
+++ src/learn_pandas.py
@@ -16,6 +16,17 @@ print(df.columns)
 print(df.dtypes)
 print(df.info())
 
+country_df = df['country']
+print(country_df.tail())
+
+subset = df[['country','continent','year']]
+
+print(df.tail(n = 1))
+print(df.loc[55])
+print(df.loc[[0, 99, 999]])
+ 
+r = list(range(3))
+print(r) 
 
 if __name__ == "__main__":
     
