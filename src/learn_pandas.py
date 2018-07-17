3d2fcd8afb706cb41af8833406ac938af64f80d4
Daniel Maxwell <dsm_direct@yahoo.com>
2018-07-17 16:46:07 -0400

    Edits (07.17.18).

--- src/learn_pandas.py
+++ src/learn_pandas.py
@@ -7,7 +7,21 @@ This is a temporary script file.
 
 import pandas as pd
 
-df = pd.read_csv('c:/informatics/gapminder.csv')
+ghog_day = eval(input('Please enter the number of groundhog days: '))
+
+for idx in range(1, ghog_day + 1):
+    print("When will this day end? I've already lived it", idx, "times!")
+    
+df = pd.read_csv('c:/informatics/gapminder.csv', index_col = 0)
+
+df_gap = df.loc['United States']
+
+df_gap = df.loc['France']
+
+df_gap = df.loc['Brazil']
+
+print(df_gap)
+
 
 print(type(df))
 
@@ -32,3 +46,7 @@ if __name__ == "__main__":
     
     df = pd.read_csv('c:/informatics/gapminder.csv')
     
+
+
+idx = 5
+print("When will this day end? I've already lived it ", idx, " times!")
