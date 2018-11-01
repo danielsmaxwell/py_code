3d13cb791d5594e1ef46b09ac0626906648f0d9a
Daniel Maxwell <dsm_direct@yahoo.com>
2018-11-01 15:22:21 -0400

    Initial commit.

---
+++ src/process_fips_file.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+countyName = 'Bristol Bay Census Area'
+
+countyName.replace('County','')
+countyName.replace('Parish','')
+countyName.replace('Municipio','')
+countyName.replace('Borough','')
+countyName.replace('Census Area','')
+countyName.replace('Municipality','')
+countyName.replace('city','')
+
