c78d0e9c7cc946d90343f2f9915eacd511123720
Daniel Maxwell <dsm_direct@yahoo.com>
2018-11-02 08:34:53 -0400

    Added county logic.

--- src/process_fips_file.py
+++ src/process_fips_file.py
@@ -4,13 +4,34 @@ Spyder Editor
 
 This is a temporary script file.
 """
-countyName = 'Bristol Bay Census Area'
 
-countyName.replace('County','')
-countyName.replace('Parish','')
-countyName.replace('Municipio','')
-countyName.replace('Borough','')
-countyName.replace('Census Area','')
-countyName.replace('Municipality','')
-countyName.replace('city','')
+clean_file = open('fips_file_clean.txt','w')
 
+for line in open('fips_file.txt'):
+    if (line.find('County') != -1):
+        new_line = line.replace('County','')
+    
+    if (line.find('Parish') != -1):
+        new_line = line.replace('Parish','')
+ 
+    if (line.find('Municipio') != -1):
+        new_line = line.replace('Municipio','')
+
+    if (line.find('Borough') != -1):
+        new_line = line.replace('Borough','')
+
+    if (line.find('Census Area') != -1):
+        new_line = line.replace('Census Area','')
+
+    if (line.find('Municipality') != -1):
+        new_line = line.replace('Municipality','')
+
+    if (line.find('city') != -1):
+        new_line = line.replace('city','')
+ 
+    clean_file.write(new_line)
+       
+    print(new_line)
+
+clean_file.close()
+  
